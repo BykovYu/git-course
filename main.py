89895c253ad533e49aa949f7d51574722d497b7a
BykovYu <bykov.yurik.2010@yandex.ru>
2023-03-18 21:46:01 +0300

    My first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import time as t
+
+os.system("cls")
+print("It is mine first project in git")
+print("Hello GIT!!")
+print("Want you to count?")
+input()
+os.system("cls")
+
+def one():
+    print("     ||")
+    print("     ||")
+    print("     ||")
+    print("     ||")
+    print("     ||")
+
+def two():
+    print("=======")
+    print("     ||")
+    print("=======")
+    print("||")
+    print("=======")
+
+def three():
+    print("=======")
+    print("     ||")
+    print("=======")
+    print("     ||")
+    print("=======")
+def four():
+    print("||   ||")
+    print("||   ||")
+    print("=======")
+    print("     ||")
+    print("     ||")
+def five():
+    print("=======")
+    print("||     ")
+    print("=======")
+    print("     ||")
+    print("=======")
+
+one()
+t.sleep(1)
+os.system("cls")
+two()
+t.sleep(1)
+os.system("cls")
+three()
+t.sleep(1)
+os.system("cls")
+four()
+t.sleep(1)
+os.system("cls")
+five()
+t.sleep(1)
+os.system("cls")
